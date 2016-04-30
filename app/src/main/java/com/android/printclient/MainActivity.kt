eb4cb0af33c4ce2a172a56ee86b3b1045445a5ec
jianglei12138 <jianglei12138@gmail.com>
2016-04-30 13:39:01 +0800

    [feature] add recyclerview layout

--- app/src/main/java/com/android/printclient/MainActivity.kt
+++ app/src/main/java/com/android/printclient/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.android.printclient
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        printer_RecyclerView.setHasFixedSize(true)
+        printer_RecyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+        printer_RecyclerView.setEmptyView(empty_LinearLayout)
+
     }
 }
 
