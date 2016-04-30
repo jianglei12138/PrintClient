69dad2d62fb65157337abd0e94b666aa4e8d41f1
jianglei12138 <jianglei12138@gmail.com>
2016-04-30 12:54:52 +0800

    [feature] add recyclerview with emptyview

---
+++ app/src/main/java/com/android/printclient/view/RecyclerViewWrapper.kt
@@ -0,0 +1,41 @@
+package com.android.printclient.view
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.util.AttributeSet
+import android.view.View
+
+class RecyclerViewWrapper : RecyclerView {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
+
+    constructor(context: Context, attributeSet: AttributeSet, defStyle: Int) : super(context, attributeSet, defStyle)
+
+    //empty view
+    private var emptyView: View? = null
+    private val emptyObserver = object : RecyclerView.AdapterDataObserver() {
+        override fun onChanged() {
+            val adapter = adapter;
+            if (adapter != null && emptyView != null) {
+                this@RecyclerViewWrapper.visibility = View.GONE
+                emptyView!!.visibility = View.VISIBLE
+            } else {
+                emptyView!!.visibility = View.GONE
+                this@RecyclerViewWrapper.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    override fun setAdapter(adapter: RecyclerView.Adapter<ViewHolder>?){
+        super.setAdapter(adapter)
+        adapter?.registerAdapterDataObserver(emptyObserver)
+        emptyObserver.onChanged()
+    }
+
+    fun setEmptyView(emptyView: View) {
+        this.emptyView = emptyView
+    }
+
+}
