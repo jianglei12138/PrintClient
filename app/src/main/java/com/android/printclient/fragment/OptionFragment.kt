166530f14fcb9b5b958f2ee4d7ea05cbaebc1aaa
jianglei12138 <jianglei12138@gmail.com>
2016-06-01 22:09:26 +0800

    [feature] add toolbar menu to tell conflict detail

--- app/src/main/java/com/android/printclient/fragment/OptionFragment.kt
+++ app/src/main/java/com/android/printclient/fragment/OptionFragment.kt
@@ -1,14 +1,16 @@
 package com.android.printclient.fragment
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.os.Handler
 import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
 import android.support.v4.view.ViewPager
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ListView
-import android.widget.SimpleAdapter
+import android.support.v7.app.AlertDialog
+import android.view.*
 import com.android.printclient.MainActivity
 import com.android.printclient.R
 import com.android.printclient.objects.Conflict
@@ -33,9 +35,10 @@ class OptionFragment : Fragment() {
 
     var tabs: TabLayout? = null
 
-    var mapConflict = ArrayList<HashMap<String, String>>()
     var listConflict = ArrayList<Conflict>()
 
+    var animatorSet = AnimatorSet()
+    var isConflict: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,17 +58,9 @@ class OptionFragment : Fragment() {
 
         var list = getOptionGroups(printer!!)
 
-        getData(printer!!)
-        var adapterConflict = SimpleAdapter(
-                context,
-                mapConflict,
-                R.layout.listview_conflicts,
-                arrayOf("text", "value"),
-                intArrayOf(R.id.text, R.id.value)
-        )
-
-        val conflictView = view.findViewById(R.id.conflict_listView) as ListView
-        conflictView.adapter = adapterConflict
+        listConflict.clear()
+        listConflict = getConflictData(printer!!)
+        if (listConflict.size > 0) isConflict = true
 
         val adapter = TabAdapter(childFragmentManager, list, printer!!, context)
 
@@ -74,24 +69,82 @@ class OptionFragment : Fragment() {
 
         tabs!!.tabMode = TabLayout.MODE_SCROLLABLE
         tabs!!.setupWithViewPager(mViewPager)
+
+        setHasOptionsMenu(true)
         return view
     }
 
-    private fun getData(name: String) {
-        listConflict.clear()
-        listConflict = getConflictData(name)
-        if (listConflict != null)
-            listConflict.forEach {
-                val map = HashMap<String, String>()
-                map.put("text", it.key!!)
-                map.put("value", it.choice!!)
-                mapConflict.add(map)
-            }
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater!!.inflate(R.menu.menu_main, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
+    override fun onPrepareOptionsMenu(menu: Menu?) {
+        if (isConflict) {
+            menu!!.findItem(R.id.action_alert).isVisible = true
+            showAlertAnimation(menu.findItem(R.id.action_alert))
+        } else {
+            menu!!.findItem(R.id.action_alert).isVisible = false
+        }
+        super.onPrepareOptionsMenu(menu)
+    }
 
-    override fun onDestroy() {
-        release()
-        super.onDestroy()
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            R.id.action_alert -> {
+
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showAlertAnimation(menuItem: MenuItem) {
+
+        hideAlertAnimation(menuItem)
+
+        var alertView = View.inflate(context, R.layout.toolbar_alert, null)
+
+        menuItem.actionView = alertView
+
+        alertView.setOnClickListener { v ->
+            v.clearAnimation()
+            var conflicts = ArrayList<String>()
+            listConflict.forEach {
+                conflicts.add(it.text + ": " + it.choice)
+            }
+            AlertDialog.Builder(context)
+                    .setTitle(context.getString(R.string.conflict_message))
+                    .setItems(conflicts.toTypedArray(), null)
+                    .setPositiveButton(context.getString(R.string.ok),null)
+                    .show()
+        }
+
+
+        var counter = 0
+        var anim1 = ObjectAnimator.ofFloat(alertView, "translationX", 0f, -4f);
+        var anim2 = ObjectAnimator.ofFloat(alertView, "translationX", 0f, 8f);
+        var anim3 = ObjectAnimator.ofFloat(alertView, "translationX", 0f, 4f);
+        animatorSet.play(anim1).before(anim2).before(anim3)
+        animatorSet.duration = 50
+        animatorSet.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                if (counter < 5) {
+                    counter++
+                    animatorSet.start()
+                } else {
+                    counter = 0
+                    Handler().postDelayed({ animatorSet.start() }, 1000)
+                }
+            }
+        })
+        animatorSet.start()
+    }
+
+    private fun hideAlertAnimation(menuItem: MenuItem) {
+        var alertView = menuItem.actionView
+        if (alertView != null)
+            alertView.clearAnimation()
+        menuItem.actionView = null
     }
 }
